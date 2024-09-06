7585dfa5e8cd3c4d4f127bd7ca1e08cfc3cd47ac
unscrud <danieldevop@gmail.com>
2024-09-06 14:57:04 +0000

    Repositorio da promocao

---
+++ src/main/java/dev/unscrud/api_milhas/domain/promocao/PromocaoRepository.java
@@ -0,0 +1,6 @@
+package dev.unscrud.api_milhas.domain.promocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromocaoRepository extends JpaRepository<Promocao,Long>{
+    
+}
