6b5db690abce832f630004e511bc433a62d8324b
unscrud <danieldevop@gmail.com>
2024-09-07 12:50:34 +0000

    Tabela com registros de exemplo

--- src/main/resources/db/migration/V1__create-table-promocao.sql
+++ src/main/resources/db/migration/V1__create-table-promocao.sql
@@ -1,6 +1,13 @@
 CREATE TABLE promocao (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(255),
-    image_path VARCHAR(255),
+    destino VARCHAR(255),
+    imagem VARCHAR(255),
     preco NUMERIC(15, 2) 
 );
+
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Atacama', 'atacama.png', 2500);
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Veneza', 'veneza.png', 1500);
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Patagônia', 'patagonia.png', 750);
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Grand Canyon', 'grand-canyon.png', 2500);
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Turquia', 'turquia.png', 1900);
+INSERT INTO promocao (destino, imagem, preco) VALUES ('Cordilheira dos Andes', 'cordilheira-dos-andes.png', 800);
