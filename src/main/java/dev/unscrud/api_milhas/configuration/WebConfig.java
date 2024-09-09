f426d326cd5e9d570ee2df47a0d4c12c34c3b749
unscrud <danieldevop@gmail.com>
2024-09-09 22:38:22 +0000

    Conf para aceitar acesso da app angular

--- src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
+++ src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
@@ -2,6 +2,7 @@ package dev.unscrud.api_milhas.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {        
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:./uploads/images/");
+    }
+
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
