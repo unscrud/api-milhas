54fe4a572a0443f4801461e05d740a5bc1759a1c
unscrud <danieldevop@gmail.com>
2024-09-13 23:13:12 +0000

    listando estados com get

--- src/main/java/dev/unscrud/api_milhas/controller/EstadoController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/EstadoController.java
@@ -1,15 +1,29 @@
 package dev.unscrud.api_milhas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.unscrud.api_milhas.domain.estado.EstadoRepository;
+import dev.unscrud.api_milhas.domain.estado.EstadoResponseDTO;
+
 @RestController
 @RequestMapping("/estado")
 public class EstadoController {
+
+    @Autowired
+    private EstadoRepository estadoRepository;
+
     @GetMapping
-    public ResponseEntity<String> listarEstados(){
-        return ResponseEntity.ok("Estados");
-    }
+    public ResponseEntity<List<EstadoResponseDTO>> listarEstados(){
+        List<EstadoResponseDTO> estados = estadoRepository
+                .findAll().stream()
+                .map(EstadoResponseDTO::new).toList();
+
+        return ResponseEntity.ok(estados);
+    } 
 }
