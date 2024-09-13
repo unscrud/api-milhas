2bd9a4cacda9b07e696f80cdbe19a1e6dc8bc9f1
unscrud <danieldevop@gmail.com>
2024-09-13 22:42:46 +0000

    Repositório para manipular estado

---
+++ src/main/java/dev/unscrud/api_milhas/domain/estado/EstadoRepository.java
@@ -0,0 +1,7 @@
+package dev.unscrud.api_milhas.domain.estado;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado,Byte> {
+
+}
