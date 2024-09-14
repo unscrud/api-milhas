04ed6ae167f3e05281ad83d989f0bb588303c0fa
unscrud <danieldevop@gmail.com>
2024-09-14 21:42:39 +0000

    buscando dados do repository

--- src/main/java/dev/unscrud/api_milhas/controller/DepoimentoController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/DepoimentoController.java
@@ -2,18 +2,28 @@ package dev.unscrud.api_milhas.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.unscrud.api_milhas.domain.depoimento.DepoimentoRepository;
 import dev.unscrud.api_milhas.domain.depoimento.DepoimentoResponseDTO;
 
 @RestController
 @RequestMapping("/depoimento")
 public class DepoimentoController {
 
+    @Autowired
+    private DepoimentoRepository depoimentoRepository;
+
     @GetMapping
-    public List<DepoimentoResponseDTO> listarDepoimentos(){
-        return null;
+    public ResponseEntity<List<DepoimentoResponseDTO>> listarDepoimentos(){
+        List<DepoimentoResponseDTO> depoimentos = depoimentoRepository
+                .findAll().stream()
+                .map(DepoimentoResponseDTO::new).toList();
+
+        return ResponseEntity.ok(depoimentos);
     }
 }
