ead261c8bbf69b6dbaa829654654056a6c5e2faa
unscrud <danieldevop@gmail.com>
2024-09-14 21:23:00 +0000

    Repositorio para lidar com depoimentos

---
+++ src/main/java/dev/unscrud/api_milhas/domain/depoimento/DepoimentoRepository.java
@@ -0,0 +1,7 @@
+package dev.unscrud.api_milhas.domain.depoimento;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepoimentoRepository extends JpaRepository<Depoimento, Long> {
+
+}
