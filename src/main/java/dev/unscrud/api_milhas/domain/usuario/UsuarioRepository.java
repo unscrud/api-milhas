8b145be37801c2ed5dd401601de0099739ab836b
unscrud <danieldevop@gmail.com>
2024-10-02 22:01:42 +0000

    Repositorio para Usuario

---
+++ src/main/java/dev/unscrud/api_milhas/domain/usuario/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package dev.unscrud.api_milhas.domain.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
