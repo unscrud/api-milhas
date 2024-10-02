79ce547ab7bbd3750610d01680920d9ced94116f
unscrud <danieldevop@gmail.com>
2024-10-02 21:43:33 +0000

    Entidade Usuario

---
+++ src/main/java/dev/unscrud/api_milhas/domain/usuario/Usuario.java
@@ -0,0 +1,38 @@
+package dev.unscrud.api_milhas.domain.usuario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuario")
+@Entity(name = "Usuario")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@EqualsAndHashCode
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull 
+    @NotEmpty
+    @Column(unique = true)
+    private String email;
+
+    @NotNull 
+    @NotEmpty
+    @Column(unique = true)
+    private String senha;
+
+}
