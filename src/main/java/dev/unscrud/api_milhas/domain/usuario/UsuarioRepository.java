3dd5c85d22908234c76d9efdede390e06e7799e3
unscrud <danieldevop@gmail.com>
2024-10-02 22:27:49 +0000

    encontra usuário por email

--- src/main/java/dev/unscrud/api_milhas/domain/usuario/UsuarioRepository.java
+++ src/main/java/dev/unscrud/api_milhas/domain/usuario/UsuarioRepository.java
@@ -1,5 +1,8 @@
 package dev.unscrud.api_milhas.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByEmail(String email);
+}
