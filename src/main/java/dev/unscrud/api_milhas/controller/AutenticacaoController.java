821dc723d2554499269e801971000f25e9c34528
unscrud <danieldevop@gmail.com>
2024-10-04 10:19:07 +0000

    Recebendo dados de autenticação

--- src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.unscrud.api_milhas.domain.usuario.DadosAutenticacao;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
     @GetMapping("/login")
-    public void efetuarLogin(){
-        System.out.println("vai fazer login");
+    public void efetuarLogin(@RequestBody DadosAutenticacao dadosAuth){
+        System.out.println(dadosAuth.email() + " - " + dadosAuth.email());
     }
 }
