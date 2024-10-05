7c0a10f7395def466b78a0771c0ac2972c140efc
unscrud <danieldevop@gmail.com>
2024-10-05 12:47:42 +0000

    Configuração do gerenciador de autenticação

--- src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
@@ -2,6 +2,8 @@ package dev.unscrud.api_milhas.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,5 +17,10 @@ public class SecurityConfiguration {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
