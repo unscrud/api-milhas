a6ab691be9d902c8e297e4b89a7056d016e57d4e
unscrud <danieldevop@gmail.com>
2024-10-10 21:42:16 +0000

    Equals e hashcode do campo id

--- src/main/java/dev/unscrud/api_milhas/domain/usuario/Usuario.java
+++ src/main/java/dev/unscrud/api_milhas/domain/usuario/Usuario.java
@@ -25,7 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@EqualsAndHashCode
+@EqualsAndHashCode(of = "id")
 public class Usuario implements UserDetails {
 
     @Id
