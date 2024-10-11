f753265e01a0f6330718cc68b34a4ebef1656c44
unscrud <danieldevop@gmail.com>
2024-10-11 12:38:36 +0000

    Gerando o token para devolver ao cliente

--- src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
@@ -1,17 +1,33 @@
 package dev.unscrud.api_milhas.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.unscrud.api_milhas.domain.usuario.DadosAutenticacao;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import dev.unscrud.api_milhas.domain.usuario.Usuario;
+import dev.unscrud.api_milhas.infra.security.TokenService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
-    @GetMapping("/login")
-    public void efetuarLogin(@RequestBody DadosAutenticacao dadosAuth){
-        System.out.println(dadosAuth.email() + " - " + dadosAuth.email());
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAuth) {
+        var token = new UsernamePasswordAuthenticationToken(dadosAuth.email(), dadosAuth.senha());
+        var authentication = manager.authenticate(token);
+
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 }
