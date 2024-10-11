8415c5e52b66f0641add01add326dbbb437e2565
unscrud <danieldevop@gmail.com>
2024-10-11 10:47:56 +0000

    Serviço para geração de token jwt

---
+++ src/main/java/dev/unscrud/api_milhas/infra/security/TokenService.java
@@ -0,0 +1,35 @@
+package dev.unscrud.api_milhas.infra.security;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import dev.unscrud.api_milhas.domain.usuario.Usuario;
+
+@Service
+public class TokenService {
+    public String gerarToken(Usuario usuario){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("12345678");
+            return JWT.create()
+                .withIssuer("api-milhas")
+                .withSubject(usuario.getUsername())
+                .withClaim("id", usuario.getId())
+                .withExpiresAt(dataExpiracao())
+                .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token: ", exception);
+        }
+    }
+
+    private Instant dataExpiracao(){
+        return LocalDateTime.now().plusHours(2)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+}
