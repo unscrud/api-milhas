383b5a62c492f925bc6b3cad0b8fb1f224347b69
unscrud <danieldevop@gmail.com>
2024-10-11 12:56:02 +0000

    Movido para infra.security

--- src/main/java/dev/unscrud/api_milhas/infra/security/AutenticacaoService.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/AutenticacaoService.java
@@ -1,10 +1,12 @@
-package dev.unscrud.api_milhas.domain.usuario;
+package dev.unscrud.api_milhas.infra.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import dev.unscrud.api_milhas.domain.usuario.UsuarioRepository;
 
 @Service
 public class AutenticacaoService implements UserDetailsService{
