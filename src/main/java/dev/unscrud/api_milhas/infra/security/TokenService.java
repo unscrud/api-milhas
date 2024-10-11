0be6b8a872109d5ea757527d90b078db197c6eb1
unscrud <danieldevop@gmail.com>
2024-10-11 19:32:21 +0000

    Busca a secret no aplication.properties

--- src/main/java/dev/unscrud/api_milhas/infra/security/TokenService.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/TokenService.java
@@ -4,6 +4,7 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
@@ -14,9 +15,12 @@ import dev.unscrud.api_milhas.domain.usuario.Usuario;
 
 @Service
 public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
     public String gerarToken(Usuario usuario){
         try {
-            Algorithm algorithm = Algorithm.HMAC256("12345678");
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
                 .withIssuer("api-milhas")
                 .withSubject(usuario.getUsername())
