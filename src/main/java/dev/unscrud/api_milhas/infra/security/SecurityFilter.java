908bf5585bd98449bf67f6d943bf475384f3c3b9
unscrud <danieldevop@gmail.com>
2024-10-13 11:50:22 +0000

    Recupera e valida tokens e autentica usuário

--- src/main/java/dev/unscrud/api_milhas/infra/security/SecurityFilter.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/SecurityFilter.java
@@ -2,9 +2,15 @@ package dev.unscrud.api_milhas.infra.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import dev.unscrud.api_milhas.domain.usuario.Usuario;
+import dev.unscrud.api_milhas.domain.usuario.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,11 +18,34 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
-        System.out.println("FILTRO CHAMADO...");
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
     
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+
+        if (tokenJWT != null){
+            String subject = tokenService.getSubject(tokenJWT);
+            Usuario usuario = (Usuario) usuarioRepository.findByEmail(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
 }
