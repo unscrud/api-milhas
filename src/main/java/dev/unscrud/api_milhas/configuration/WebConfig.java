a1cd3628960a6700c44d2f892ba15fd8ecc1680b
unscrud <danieldevop@gmail.com>
2024-10-24 16:46:11 +0000

    Movendo a configuração de cors para classe de segurança

--- src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
+++ src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
@@ -2,25 +2,14 @@ package dev.unscrud.api_milhas.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {        
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("file:./uploads/images/");
     }
-
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
 }
