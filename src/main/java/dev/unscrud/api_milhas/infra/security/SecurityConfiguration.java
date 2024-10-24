47d05b35fce536201fd658344d88d16e4986e15a
unscrud <danieldevop@gmail.com>
2024-10-24 16:43:22 +0000

    configuração de cors e de endpoints liberados

--- src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package dev.unscrud.api_milhas.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -21,14 +24,32 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
+        return 
+            http.csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(request -> {
+                var corsConfig = new CorsConfiguration();
+                corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
+                corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+                corsConfig.setAllowedHeaders(List.of("*"));
+                return corsConfig;
+            }))
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(req -> {
-                req.requestMatchers("/auth/login").permitAll();
+                req.requestMatchers(
+                    "/auth/login",
+                    "/depoimento",
+                    "/estado",
+                    "/promocao",
+                    "/images/**",
+                    "/v3/api-docs/**",
+                    "/swagger-ui.html",
+                    "/swagger-ui/**"
+                ).permitAll();
+                req.requestMatchers(HttpMethod.POST, "/usuarios").permitAll();
                 req.anyRequest().authenticated();
             })
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+        .build();
     }
 
     @Bean
