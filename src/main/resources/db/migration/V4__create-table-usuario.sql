9d4e798aaa716f31c487aaceadf844769f67e302
unscrud <danieldevop@gmail.com>
2024-10-26 13:21:10 -0300

    novos campos para a tabela de usuario

--- src/main/resources/db/migration/V4__create-table-usuario.sql
+++ src/main/resources/db/migration/V4__create-table-usuario.sql
@@ -1,5 +1,14 @@
+CREATE TYPE genero_enum AS ENUM ('masculino', 'feminino', 'outro');
 CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    nascimento DATE,
+    telefone  VARCHAR(20),
+    genero genero_enum NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL
+    senha VARCHAR(255) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    estado_id INT NOT NULL,
+    CONSTRAINT fk_estado FOREIGN KEY (estado_id) REFERENCES estado(id)
 );
