8302765a42ea862c4f5cef688a86f9af9983d3ee
unscrud <danieldevop@gmail.com>
2024-10-29 00:49:19 +0000

    DTO para recebimento de estado em um cadastro de usuário

---
+++ src/main/java/dev/unscrud/api_milhas/domain/estado/DadosEstado.java
@@ -0,0 +1,14 @@
+package dev.unscrud.api_milhas.domain.estado;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record DadosEstado(
+    @NotBlank (message = "O id do estado é obrigatorio")
+    String id,
+
+    @NotBlank (message = "O nome do estado é obrigatorio")
+    String nome,
+
+    @NotBlank (message = "A sigla do estado é obrigatoria")
+    String sigla
+){}
