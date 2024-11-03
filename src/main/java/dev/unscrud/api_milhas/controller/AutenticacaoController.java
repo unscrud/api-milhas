7492ef2660ff570855319603b303919df00629ff
unscrud <danieldevop@gmail.com>
2024-11-03 14:14:39 +0000

    Altera os dados do usuário logado

--- src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/AutenticacaoController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -59,5 +60,11 @@ public class AutenticacaoController {
         Usuario usuario = usuarioServico.getUsuarioLogado();
         DadosCadastro dadosCadastro = new DadosCadastro(usuario);
         return ResponseEntity.ok(dadosCadastro);
+    }
+
+    @PatchMapping("/perfil")
+    @SecurityRequirement(name = "bearer-key")
+    public ResponseEntity<Void> editarUsusario(){
+        return ResponseEntity.ok().build();
     }
 }
