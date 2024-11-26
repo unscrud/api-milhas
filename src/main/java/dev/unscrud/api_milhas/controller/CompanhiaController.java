f8d710bb8fdc5b56adc28166e648ea84869b8b77
unscrud <danieldevop@gmail.com>
2024-11-26 18:48:09 +0000

    Buscando companhias no banco

--- src/main/java/dev/unscrud/api_milhas/controller/CompanhiaController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/CompanhiaController.java
@@ -1,17 +1,30 @@
 package dev.unscrud.api_milhas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import dev.unscrud.api_milhas.domain.companhia.CompanhiaRepository;
+import dev.unscrud.api_milhas.domain.companhia.CompanhiaResponseDTO;
 
 @RestController
 @RequestMapping("/companhia")
 public class CompanhiaController {
 
+    @Autowired
+    private CompanhiaRepository companhiaRepository;
+
     @GetMapping
-    public String getCompanhias() {
-        String  teste = "teste";
-        return teste;
+    public ResponseEntity<List<CompanhiaResponseDTO>> getCompanhias() {
+        List<CompanhiaResponseDTO> companhias = companhiaRepository
+                .findAll().stream()
+                .map(CompanhiaResponseDTO::new).toList();
+
+        return ResponseEntity.ok(companhias);
     }
 
 }
