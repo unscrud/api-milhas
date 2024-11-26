8d7a27cce767df1f4b6a0af1bb0c63465be7bb8b
unscrud <danieldevop@gmail.com>
2024-11-26 17:42:24 +0000

    Permite Buscar companhias sem estar logado

--- src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
+++ src/main/java/dev/unscrud/api_milhas/infra/security/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration {
                 req.requestMatchers(
                     HttpMethod.GET,
                     "/depoimento",
+                    "/companhia",
                     "/estado",
                     "/promocao",
                     "/images/**",
