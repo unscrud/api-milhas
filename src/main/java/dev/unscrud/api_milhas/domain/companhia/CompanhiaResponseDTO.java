3ab8b93dd04b7ef9c8e7f397e040de1ee9e86e25
unscrud <danieldevop@gmail.com>
2024-11-26 18:47:43 +0000

    DTO de companhia

--- src/main/java/dev/unscrud/api_milhas/domain/companhia/CompanhiaResponseDTO.java
+++ src/main/java/dev/unscrud/api_milhas/domain/companhia/CompanhiaResponseDTO.java
@@ -1,3 +1,10 @@
 package dev.unscrud.api_milhas.domain.companhia;
 
-public record CompanhiaResponseDTO(Integer id, String nome) {}
+public record CompanhiaResponseDTO(Integer id, String nome) {
+    public CompanhiaResponseDTO(Companhia companhia) {
+        this(
+            companhia.getId(), 
+            companhia.getNome()
+        );
+    }
+}
