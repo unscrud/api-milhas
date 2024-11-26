82f8d6d571cd76cae1665e34211941f4ff7bf333
unscrud <danieldevop@gmail.com>
2024-11-26 17:58:17 +0000

    Repositório de companhias

---
+++ src/main/java/dev/unscrud/api_milhas/domain/companhia/CompanhiaRepository.java
@@ -0,0 +1,7 @@
+package dev.unscrud.api_milhas.domain.companhia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanhiaRepository extends JpaRepository<Companhia, Integer> {
+
+}
