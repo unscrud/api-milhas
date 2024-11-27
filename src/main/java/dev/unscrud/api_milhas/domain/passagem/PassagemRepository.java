276d5409a81d840b6aa1afe6e1c586efbbe1d029
unscrud <danieldevop@gmail.com>
2024-11-27 16:18:21 +0000

    Repositório de passagem

---
+++ src/main/java/dev/unscrud/api_milhas/domain/passagem/PassagemRepository.java
@@ -0,0 +1,7 @@
+package dev.unscrud.api_milhas.domain.passagem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PassagemRepository extends JpaRepository<Passagem, Long> {
+
+}
