6befe245ae36fcd10b977e4b7c1ff8776f1e496d
unscrud <danieldevop@gmail.com>
2024-12-04 16:26:27 +0000

    Controller de passagens

--- src/main/java/dev/unscrud/api_milhas/controller/PassagemController.java
+++ src/main/java/dev/unscrud/api_milhas/controller/PassagemController.java
@@ -1,19 +1,27 @@
 package dev.unscrud.api_milhas.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.unscrud.api_milhas.domain.passagem.DadosBuscaPassagensDTO;
+import dev.unscrud.api_milhas.domain.passagem.Passagem;
+import dev.unscrud.api_milhas.domain.passagem.PassagemServico;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/passagem")
 public class PassagemController {
 
+    @Autowired
+    private PassagemServico passagemServico;
+
     @GetMapping("/search")
-    public String buscarPassagens(@ModelAttribute @Valid DadosBuscaPassagensDTO dadosBusca) {
-        return dadosBusca.toString();
+    public List<Passagem> buscarPassagens(@ModelAttribute @Valid DadosBuscaPassagensDTO dadosBusca) {
+        return passagemServico.listarPorFiltro(dadosBusca);
     }
 }
