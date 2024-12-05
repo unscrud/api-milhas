bf3ce29ec3e2edd2e1acebdbb74c2b2f2b756042
unscrud <danieldevop@gmail.com>
2024-12-05 12:11:42 +0000

    Habilita suporte a paginação e serialização com Spring Data

--- src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
+++ src/main/java/dev/unscrud/api_milhas/configuration/WebConfig.java
@@ -1,11 +1,14 @@
 package dev.unscrud.api_milhas.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {        
