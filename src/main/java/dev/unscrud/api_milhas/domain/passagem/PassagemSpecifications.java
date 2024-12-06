3db8834b94a08dea8f7ad66efb0dc982c9aee2d6
unscrud <danieldevop@gmail.com>
2024-12-06 00:18:56 +0000

    Realiza a busca pelo tipo de passagem

--- src/main/java/dev/unscrud/api_milhas/domain/passagem/PassagemSpecifications.java
+++ src/main/java/dev/unscrud/api_milhas/domain/passagem/PassagemSpecifications.java
@@ -1,21 +1,31 @@
 package dev.unscrud.api_milhas.domain.passagem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.data.jpa.domain.Specification;
 
-import dev.unscrud.api_milhas.domain.companhia.Companhia;
-import dev.unscrud.api_milhas.domain.estado.Estado;
+// import dev.unscrud.api_milhas.domain.companhia.Companhia;
+// import dev.unscrud.api_milhas.domain.estado.Estado;
 import jakarta.persistence.criteria.*;
 
 public class PassagemSpecifications {
     public static Specification<Passagem> comJoinsEFiltro(DadosBuscaPassagensDTO dadosBusca) {
         return (Root<Passagem> root, CriteriaQuery<?> query, CriteriaBuilder builder) -> {
-            Join<Passagem, Estado> origemJoin = root.join("origem", JoinType.LEFT);
-            Join<Passagem, Estado> destinoJoin = root.join("destino", JoinType.LEFT);
-            Join<Passagem, Companhia> companhiaJoin = root.join("companhia", JoinType.LEFT);
+            // Join<Passagem, Estado> origemJoin = root.join("origem", JoinType.LEFT);
+            // Join<Passagem, Estado> destinoJoin = root.join("destino", JoinType.LEFT);
+            // Join<Passagem, Companhia> companhiaJoin = root.join("companhia", JoinType.LEFT);
 
-            //Predicate[] predicates = new Predicate[0];
+            List<Predicate> predicates = new ArrayList<>();
+            
+            if (dadosBusca.tipo() != null && !dadosBusca.tipo().isEmpty()) {
+                TipoPassagem tipoPassagem = TipoPassagem.valueOf(dadosBusca.tipo());
+                var enumTipoPassagem = root.get("tipoPassagem").as(String.class);
+                Predicate predicateTipo = builder.equal(enumTipoPassagem, tipoPassagem.name());
+                predicates.add(predicateTipo);
+            }
 
-            return builder.conjunction();//.and(predicates);
+            return builder.and(predicates.toArray(new Predicate[0]));
         };
     }
 }
