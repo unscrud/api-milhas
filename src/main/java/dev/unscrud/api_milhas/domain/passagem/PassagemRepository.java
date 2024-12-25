b7d1da2ff383c519ad121bf3b9d882122b630e74
unscrud <danieldevop@gmail.com>
2024-12-25 01:28:03 +0000

    Query para retornar o preço mínimo e máximo

--- src/main/java/dev/unscrud/api_milhas/domain/passagem/PassagemRepository.java
+++ src/main/java/dev/unscrud/api_milhas/domain/passagem/PassagemRepository.java
@@ -2,5 +2,18 @@ package dev.unscrud.api_milhas.domain.passagem;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface PassagemRepository extends JpaRepository<Passagem, Long>, JpaSpecificationExecutor<Passagem> {}
+@Repository
+public interface PassagemRepository extends JpaRepository<Passagem, Long>, JpaSpecificationExecutor<Passagem> {
+    @Query("""
+        SELECT new dev.unscrud.api_milhas.domain.passagem.PrecoDTO ( 
+            MIN(p.precoIda), 
+            MAX(p.precoIda)
+        ) 
+        FROM Passagem p
+        WHERE p.precoIda > 0
+    """)
+    PrecoDTO findPrecoIdaMinMax();
+}
